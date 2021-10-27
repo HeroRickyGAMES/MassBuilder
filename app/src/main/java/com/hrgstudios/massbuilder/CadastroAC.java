da7064b7cb2ff6d01d5fa2b0bf55a0d3d8922d5e
HeroRickyGAMES <ricojn9@gmail.com>
2021-10-27 12:00:55 -0700

    Iniciei alguns metodos de cadastro do Usuario porem ainda não está finalizado!

--- app/src/main/java/com/hrgstudios/massbuilder/CadastroAC.java
+++ app/src/main/java/com/hrgstudios/massbuilder/CadastroAC.java
@@ -5,13 +5,60 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class CadastroAC extends AppCompatActivity {
+
+    //comentei sobre eles para colocar-los aqui, não sei explicar direito mais fds kkkkk
+    private EditText Editnome, EditEmailc, EditSenhac;
+    private Button Bt_cadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        //IDS dos edittexts da tela
+        Editnome = findViewById(R.id.Editnome);
+        EditEmailc = findViewById(R.id.EditEmailc);
+        EditSenhac = findViewById(R.id.EditSenhac);
+
+        //ID do botão
+        Bt_cadastro = findViewById(R.id.Bt_cadastro);
+
+
+        //Ouvir eventos de click pelo botão
+        Bt_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nome = Editnome.getText().toString();
+                String email = EditEmailc.getText().toString();
+                String senha = EditSenhac.getText().toString();
+
+
+                if(nome.isEmpty() || email.isEmpty() || senha.isEmpty()){
+
+                    Snackbar snackbar = Snackbar.make(v, "Preencha todos os campos!", Snackbar.LENGTH_LONG);
+                    snackbar.show();
+                }else{
+
+                }
+            }
+        });
+    }
+
+
+    //Metodo de cadastro do Usuario lá no banco de dados
+    private void CadastrarUsuario(){
+        String nome = Editnome.getText().toString();
+        String email = EditEmailc.getText().toString();
+        String senha = EditSenhac.getText().toString();
+
+
+
     }
 
     public void loginac(View view){
